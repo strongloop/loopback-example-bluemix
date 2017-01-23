65673b3d4f0ac743a27bb3ec23a80296f9050978
Raymond Feng <rfeng@us.ibm.com>
2017-01-23 14:45:02 -0800

    Tidy up cloudant config

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,17 +1,21 @@
 var _ = require('lodash');
+// Check attached CloudFoundary services
 var services = process.env.VCAP_SERVICES;
 if (typeof services === 'string') {
   services = JSON.parse(services);
 } else {
-  services = [];
+  services = {};
 }
 var dataSources = {};
 for (var s in services) {
   var serviceList = services[s];
+  // Each key has an array of configuration objects
   for (var i = 0, n = serviceList.length; i < n; i++) {
     var service = serviceList[i];
+    // We need a better way to filter services of interest
     if (s.indexOf('postgresql') !== -1) {
       dataSources[s] = _.cloneDeep(service.credentials);
+      // We need a better way to map service configuration to data source
       dataSources[s].url = dataSources[s].uri;
       dataSources[s].connector = 'postgresql';
     }
@@ -22,5 +26,5 @@ for (var s in services) {
     }
   }
 }
-console.log(dataSources);
+console.log('Bluemix datasources: ', dataSources);
 module.exports = dataSources;
