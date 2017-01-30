d9c4beb258262dffc3be0c39386b3f2ab44a081d
Raymond Feng <rfeng@us.ibm.com>
2017-01-30 15:14:32 -0800

    Use NODE_ENV=bluemix

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,31 +1,12 @@
 'use strict';
-var _ = require('lodash');
-// Check attached CloudFoundary services
-var services = process.env.VCAP_SERVICES;
-if (typeof services === 'string') {
-  services = JSON.parse(services);
+if (process.env.VCAP_SERVICES) {
+  module.exports = {};
 } else {
-  services = {};
-}
-var dataSources = {};
-for (var s in services) {
-  var serviceList = services[s];
-  // Each key has an array of configuration objects
-  for (var i = 0, n = serviceList.length; i < n; i++) {
-    var service = serviceList[i];
-    // We need a better way to filter services of interest
-    if (s.indexOf('postgresql') !== -1) {
-      dataSources[s] = _.cloneDeep(service.credentials);
-      // We need a better way to map service configuration to data source
-      dataSources[s].url = dataSources[s].uri;
-      dataSources[s].connector = 'postgresql';
+  // Mock up cloudantNoSQLDB for non-Bluemix env
+  module.exports = {
+    cloudantNoSQLDB: {
+      name: 'cloudantNoSQLDB',
+      connector: 'memory'
     }
-    if (s.indexOf('cloudant') !== -1) {
-      dataSources[s] = _.cloneDeep(service.credentials);
-      dataSources[s].database = 'loopback';
-      dataSources[s].connector = 'cloudant';
-    }
-  }
+  };
 }
-console.log('Bluemix datasources: ', dataSources);
-module.exports = dataSources;
