c766533599e2194714c287667000ff2ddece4565
Agnes Lin <agneslin.lin@ibm.com>
2019-05-07 14:28:21 -0400

    add back removed MIT license info

--- test/smoke.js
+++ test/smoke.js
@@ -1,5 +1,7 @@
 // Copyright IBM Corp. 2017. All Rights Reserved.
 // Node module: loopback-example-bluemix
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
 
 'use strict';
 var test = require('tape');
